4ee989e8a5c6ff5bbaa9588eceb0179af07ffd57
Nikos Epitropakis <n.epitro@gmail.com>
2023-03-13 13:55:14 +0200

    init

---
+++ fastserver.py
@@ -0,0 +1,36 @@
+import uvicorn
+from fastapi import FastAPI, Request, Response
+import logging
+import os
+from viberbot import Api
+from viberbot.api.bot_configuration import BotConfiguration
+from viberbot.api.messages.text_message import TextMessage
+from viberbot.api.viber_requests import ViberFailedRequest
+from viberbot.api.viber_requests import ViberMessageRequest
+from viberbot.api.viber_requests import ViberSubscribedRequest
+
+app = FastAPI()
+
+viber = Api(BotConfiguration(
+    name='hotilitieschat',
+    avatar='',
+    auth_token=os.environ['TOKEN']
+    # auth_token="test"
+))
+
+
+@app.post("/")
+async def incoming(request: Request):
+    if not viber.verify_signature(request.json(), request.headers.get('X-Viber-Content-Signature')):
+        return Response(status_code=403)
+    else:
+        return Response(status_code=200, content={"message": request.json()})
+
+
+@app.get("/status")
+async def status():
+    return Response(status_code=200, content="Running...")
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, )
