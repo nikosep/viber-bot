7cf260373b8aaed7ccbe62b22a313a56f42c609e
Nikos Epitropakis <n.epitro@gmail.com>
2023-03-13 15:46:52 +0200

    get the request and echo the same message test

--- fastserver.py
+++ fastserver.py
@@ -2,7 +2,6 @@ import uvicorn
 from fastapi import FastAPI, Request, Response
 import logging
 import os
-
 from starlette.middleware.cors import CORSMiddleware
 from viberbot import Api
 from viberbot.api.bot_configuration import BotConfiguration
@@ -37,6 +36,9 @@ viber = Api(BotConfiguration(
 async def incoming(request: Request):
     body = await request.body()
     logging.info(f"received request. post data: {body}")
+    viber_request = viber.parse_request(body)
+    message = viber_request.message
+    viber.send_messages(viber_request.sender.id, [message])
     return Response(status_code=200)
     # if not viber.verify_signature(request.json(), request.headers.get('X-Viber-Content-Signature')):
     #     return Response(status_code=403)
@@ -49,5 +51,5 @@ async def status():
     return Response(status_code=200, content="Running...")
 
 
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="0.0.0.0", port=8000, )
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, )
